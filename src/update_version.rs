1136b056642b3b1d09a2919a7b7f704b1eef4987
Ivo <ivotesligte@gmail.com>
2023-01-05 19:39:11 +0100

    bugfix

--- src/update_version.rs
+++ src/update_version.rs
@@ -7,7 +7,6 @@ pub fn update_version() -> Result<bool, Box<dyn Error>> {
     let status = github::Update::configure()
         .repo_owner("IvoteSligte")
         .repo_name("ball_game")
-        .bin_path_in_archive("ball_game/ball_game")
         .bin_name("ball_game")
         .current_version(cargo_crate_version!())
         .no_confirm(true)
