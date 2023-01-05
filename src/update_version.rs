5a768e7af39fab92603222b85a2013a377959499
Ivo <ivotesligte@gmail.com>
2023-01-05 19:49:09 +0100

    bugfix

--- src/update_version.rs
+++ src/update_version.rs
@@ -5,8 +5,8 @@ use self_update::{backends::github, cargo_crate_version, Status};
 /// Updates the app, returns true if it has been updated, false if it has not.
 pub fn update_version() -> Result<bool, Box<dyn Error>> {
     let status = github::Update::configure()
-        .repo_owner("IvoteSligte/contents")
-        .repo_name("ball_game")
+        .repo_owner("IvoteSligte")
+        .repo_name("ball_game/contents")
         .bin_name("ball_game")
         .current_version(cargo_crate_version!())
         .no_confirm(true)
